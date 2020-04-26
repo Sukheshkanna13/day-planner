9e6ce5bc56736ade17ad80efab3a53ce30382773
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-04-26 09:22:15 -0400

    added moment.js, some basic JS in script.js

---
+++ assets/javascript/script.js
@@ -0,0 +1,20 @@
+//Declaring and initializing global variables
+let timeBlocks = []
+
+class timeBlock {
+  constructor(aTime,aLaterTime){
+    this.start = aTime;
+    this.end = aLaterTime;
+    this.hasEvent = false
+    this.title = ''
+    this.description = ''
+  }
+}
+
+function timeBlockManager(){
+
+}
+
+function timeBlockDisplayer(){
+
+}
