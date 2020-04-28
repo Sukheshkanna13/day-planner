e6be16901c14fc196fe0af7bb7d16df8e264f4a4
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-04-28 14:54:02 -0400

    more pseudocoding

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,7 @@
 const ourMomentInstance = moment()
 
 //define some global variables
+let timesArr = [9,10,11,12,13,14,15,16,17]
 
 //define some functions
 
@@ -10,7 +11,15 @@ const updateHeaderWithDay = () => {
   $('#currentDay').text(ourMomentInstance.format('dddd, MMMM Do'))
 }
 
-const createAllTimeBlocks = () => {
+const convert24Hto12H = (aNum) => {
+  return aNum > 12? [aNum - 12, true] : [aNum, false]
+}
+
+const convert12Hto24H = (aNum,afterNoon) => {
+  return afterNoon? aNum + 12 : aNum
+}
+
+const createAllTimeBlocks = (timesArr) => {
 // create some # of time blocks, from X time to Y time
 // give each timeblock the row and timeblock classes
 // create 3 cols in the time block
@@ -24,7 +33,16 @@ const createAllTimeBlocks = () => {
 // and a col-1 for the save button
 }
 
-const updateAllTimeBlocks = () => {
+const updateEverything = () => {
+// this function will be placed inside a setInterval
+// update the day
+updateHeaderWithDay()
+// update each timeblock, depending on whether it is now in the past, present, or future
+// use jQuery to get the list of timeblocks
+// use.map() to apply an updater function to each timeblock
+}
+
+const updateTimeBlockColor = (aTimeBlock) => {
 
 }
 
